85520bb25fc7bafd240f93593a161adf58eeda27
Sw1ftFox <v.suvorkin2017@gmail.com>
2023-09-07 14:21:52 +0300

    adding 16 task

---
+++ Задание 16 (Методы массивов)/main.js
@@ -0,0 +1,236 @@
+// function camelize(str) {
+//   return str
+//     .split('-') // разбивает 'my-long-word' на массив ['my', 'long', 'word']
+//     .map(
+//       // Переводит в верхний регистр первые буквы всех элементом массива за исключением первого
+//       // превращает ['my', 'long', 'word'] в ['my', 'Long', 'Word']
+//       (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+//     )
+//     .join(''); // соединяет ['my', 'Long', 'Word'] в 'myLongWord'
+// }
+
+// alert(camelize('my-long-word'));
+
+
+// function filterRange(arr, a, b) {
+//   return arr.filter(item => (a <= item && item <= b));
+// }
+
+// let arr = [5, 3, 8, 1];
+
+// let filtered = filterRange(arr, 1, 4);
+
+// alert( filtered ); // 3,1 (совпадающие значения)
+
+// alert( arr ); // 5,3,8,1 (без изменений)
+
+
+// function filterRangeInPlace(arr, a, b) {
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let val = arr[i];
+
+//     // удалить, если за пределами интервала
+//     if (val < a || val > b) {
+//       arr.splice(i, 1);
+//       i--;
+//     }
+//   }
+
+// }
+
+// let arr = [5, 3, 8, 1];
+
+// filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+
+// alert( arr ); // [3, 1]
+
+
+// let arr = [5, 2, 1, -10, 8];
+
+// arr.sort((a, b) => b - a);
+
+// alert( arr );
+
+
+// function copySorted(arr) {
+//   return arr.slice().sort( (a, b) => a.localeCompare(b) );
+// }
+
+// let arr = ["HTML", "JavaScript", "CSS"];
+
+// let sorted = copySorted(arr);
+
+// alert( sorted ); // CSS, HTML, JavaScript
+// alert( arr ); // HTML, JavaScript, CSS (без изменений)
+
+
+// function Calculator() {
+
+//   this.methods = {
+//     "-": (a, b) => a - b,
+//     "+": (a, b) => a + b
+//   };
+
+//   this.calculate = function(str) {
+
+//     let split = str.split(' '),
+//       a = +split[0],
+//       op = split[1],
+//       b = +split[2]
+
+//     if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+//       return NaN;
+//     }
+
+//     return this.methods[op](a, b);
+//   }
+
+//   this.addMethod = function(name, func) {
+//     this.methods[name] = func;
+//   };
+// }
+
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let users = [ vasya, petya, masha ];
+
+// let names = users.map(( user ) => user.name);
+
+// alert( names ); // Вася, Петя, Маша
+
+
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+// let users = [ vasya, petya, masha ];
+
+// let usersMapped = users.map(user => ({
+//     fullName: `${user.name} ${user.surname}`,
+//     id: user.id
+//   }));
+
+// /*
+// usersMapped = [
+//   { fullName: "Вася Пупкин", id: 1 },
+//   { fullName: "Петя Иванов", id: 2 },
+//   { fullName: "Маша Петрова", id: 3 }
+// ]
+// */
+
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // Вася Пупкин
+
+
+// function sortByAge(arr) {
+//     arr.sort((a,b) => a.age - b.age);
+// }
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let arr = [ vasya, petya, masha ];
+
+// sortByAge(arr);
+
+// // теперь: [vasya, masha, petya]
+// alert(arr[0].name); // Вася
+// alert(arr[1].name); // Маша
+// alert(arr[2].name); // Петя
+
+
+// function shuffle(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//         let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+    
+//         // поменять элементы местами
+//         // мы используем для этого синтаксис "деструктурирующее присваивание"
+//         // подробнее о нём - в следующих главах
+//         // то же самое можно записать как:
+//         // let t = array[i]; array[i] = array[j]; array[j] = t
+//         [array[i], array[j]] = [array[j], array[i]];
+//     }
+// }
+
+
+// let arr = [1, 2, 3];
+
+// shuffle(arr);
+// // arr = [3, 2, 1]
+
+// shuffle(arr);
+// // arr = [2, 1, 3]
+
+// shuffle(arr);
+// // arr = [3, 1, 2]
+
+
+// function getAverageAge(users) {
+//     let sum = 0;
+
+//     for (let i = 0; i < users.length; i++) {
+//         sum += users[i].age;
+//     }
+
+//     return sum / users.length;
+// }
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 29 };
+
+// let arr = [ vasya, petya, masha ];
+
+// alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+
+// function unique(arr) {
+//     let result = [];
+  
+//     for (let str of arr) {
+//       if (!result.includes(str)) {
+//         result.push(str);
+//       }
+//     }
+  
+//     return result;
+//   }
+  
+//   let strings = ["кришна", "кришна", "харе", "харе",
+//     "харе", "харе", "кришна", "кришна", ":-O"
+//   ];
+  
+//   alert( unique(strings) ); // кришна, харе, :-O
+
+
+function groupById(array) {
+    return array.reduce((obj, value) => {
+      obj[value.id] = value;
+      return obj;
+    }, {})
+  }
+
+let users = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
+  ];
+  
+  let usersById = groupById(users);
+
+  alert(usersById);
+  
+  /*
+  // после вызова у нас должно получиться:
+  
+  usersById = {
+    john: {id: 'john', name: "John Smith", age: 20},
+    ann: {id: 'ann', name: "Ann Smith", age: 24},
+    pete: {id: 'pete', name: "Pete Peterson", age: 31},
+  }
+  */
